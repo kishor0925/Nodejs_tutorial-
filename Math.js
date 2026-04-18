96a3d53b65052967e8a44e2c56fd74b42a2f6d47
kishor0925 <kishor25092004@gmail.com>
2026-04-18 23:55:50 +0530

    node js

---
+++ Math.js
@@ -0,0 +1,13 @@
+// const add = (a,b,) => a + b;
+// const sub = (a,b,) => a - b;
+// const mul = (a,b,) => a * b;
+// const mod = (a,b,) => a % b;
+// const div = (a, b) => a / b;
+
+// module.exports = {
+//     add,
+//     sub,
+//     mul,
+//     modulos : mod,
+//     division : div
+// }
---
+++ server.js
@@ -0,0 +1,12 @@
+
+// const os = require('os');
+
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
+
+// const path = require('path');
+// const math = require('./Math')
+
+// console.log(math.modulos(5,3));
+
